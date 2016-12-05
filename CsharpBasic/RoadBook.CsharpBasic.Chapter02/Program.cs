caf6b61a68d5318900574829581c355a50952ba0
korcosin <korcosin@gmail.com>
2016-12-05 15:13:52 +0900

    챕터2장 예제 소스 추가

---
+++ CsharpBasic/RoadBook.CsharpBasic.Chapter02/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace RoadBook.CsharpBasic.Chapter02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Examples.Variable variable1 = new Examples.Variable();
+
+            variable1.run();
+        }
+    }
+}
---
+++ CsharpBasic/RoadBook.CsharpBasic.Chapter02/Works/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RoadBook.CsharpBasic.Chapter02.Works
+{
+    public class Calculator
+    {
+        public void run()
+        {
+            int number1 = 0;
+            int number2 = 0;
+
+            Console.Write("첫 번째 숫자를 입력해주세요 : ");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("두 번째 숫자를 입력해주세요 : ");
+            number2 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("더한 값 : {0}", number1 + number2);
+            Console.WriteLine("뺀 값 : {0}", number1 - number2);
+            Console.WriteLine("곱한 값 : {0}", number1 * number2);
+            Console.WriteLine("나눈 값 : {0}", (double)number1 / (double)number2);
+        }
+    }
+}
