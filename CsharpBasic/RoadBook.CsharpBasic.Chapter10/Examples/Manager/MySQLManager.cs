e97049a06b771bced3a0c4214afb565f7fd1e535
korcosin <korcosin@gmail.com>
2016-12-29 13:35:32 +0900

    챕터10장 예제 소스 추가

---
+++ CsharpBasic/RoadBook.CsharpBasic.Chapter10/Examples/Manager/MySQLManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Data;
+using RoadBook.CsharpBasic.Chapter10.Examples.Model;
+
+namespace RoadBook.CsharpBasic.Chapter10.Examples.Manager
+{
+    public class MySQLManager : IDBManager
+    {
+        public void open(DBInfo dbInfo)
+        {
+            //TODO
+        }
+
+        public DataTable select(string sql)
+        {
+            //TODO
+            return new DataTable();
+        }
+
+        public int insert(string sql)
+        {
+            //TODO
+            return 0;
+        }
+
+        public int update(string sql)
+        {
+            //TODO
+            return 0;
+        }
+
+        public int delete(string sql)
+        {
+            //TODO
+            return 0;
+        }
+
+        public void close()
+        {
+            //TODO
+        }
+    }
+}
---
+++ CsharpBasic/RoadBook.CsharpBasic.Chapter10/Examples/Model/DBInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace RoadBook.CsharpBasic.Chapter10.Examples.Model
+{
+    public class DBInfo
+    {
+        public string IP { get; set; }
+        public int PORT { get; set; }
+        public string DBNAME { get; set; }
+        public string USERID { get; set; }
+        public string USERPW { get; set; }
+    }
+}
