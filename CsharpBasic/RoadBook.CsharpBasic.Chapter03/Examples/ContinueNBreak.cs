b142742209de1228ac399affa39de64680cede16
korcosin <korcosin@gmail.com>
2016-12-29 09:53:02 +0900

    챕터3장 예제 소스 추가

---
+++ CsharpBasic/RoadBook.CsharpBasic.Chapter03/Examples/ContinueNBreak.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RoadBook.CsharpBasic.Chapter03.Examples
+{
+    public class ContinueNBreak
+    {
+        public void run()
+        {
+            int sum = 0;
+
+            for (int idx = 0; idx < 20; idx++)
+            {
+                if (idx % 2 == 0)
+                {
+                    continue;
+                }
+
+                if (idx > 9)
+                {
+                    break;
+                }
+
+                sum += idx;
+            }
+
+            Console.WriteLine("9 이하의 홀수 합 : {0}", sum);
+        }
+    }
+}
