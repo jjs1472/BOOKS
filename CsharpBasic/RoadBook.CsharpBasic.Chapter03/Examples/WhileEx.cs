861f0b3d51b4e8234fe9c4d88e4b8df579de11dd
korcosin <korcosin@gmail.com>
2016-12-29 09:53:02 +0900

    3장 예제 추가

---
+++ CsharpBasic/RoadBook.CsharpBasic.Chapter03/Examples/WhileEx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RoadBook.CsharpBasic.Chapter03.Examples
+{
+    public class WhileEx
+    {
+        public void run()
+        {
+            int sum = 0;
+            int number = 1;
+
+            while (number <= 10)
+            {
+                sum += number;
+
+                number++;
+            }
+
+            Console.WriteLine("1부터 10까지의 합 {0}", sum);
+        }
+    }
+}
