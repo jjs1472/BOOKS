d0c12230cf3238f7500a442618b4e375d3e99d79
dexter.jung <korcosin@gmail.com>
2019-01-12 21:46:54 +0900

    add chapter04 source code

--- CsharpBasic/RoadBook.CsharpBasic.Chapter04/Program.cs
+++ CsharpBasic/RoadBook.CsharpBasic.Chapter04/Program.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RoadBook.CsharpBasic.Chapter04
+﻿namespace RoadBook.CsharpBasic.Chapter04
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Examples.Weather03 ex = new Examples.Weather03();
+            Examples.Ex001 ex = new Examples.Ex001();
 
-            ex.run();
+            ex.Run();
         }
     }
 }
