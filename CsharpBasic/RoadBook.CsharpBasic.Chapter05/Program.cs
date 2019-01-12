ca991fb869a448388b82259f43aa389993e103cc
dexter.jung <korcosin@gmail.com>
2019-01-12 21:48:42 +0900

    add chapter05 source code

--- CsharpBasic/RoadBook.CsharpBasic.Chapter05/Program.cs
+++ CsharpBasic/RoadBook.CsharpBasic.Chapter05/Program.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RoadBook.CsharpBasic.Chapter05
+﻿namespace RoadBook.CsharpBasic.Chapter05
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Examples.Commons ex01 = new Examples.Commons();
+            Examples.Ex001 ex = new Examples.Ex001();
 
-            ex01.run();
+            ex.Run();
         }
     }
 }
