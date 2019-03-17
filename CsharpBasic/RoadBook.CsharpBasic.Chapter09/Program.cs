bfa61bc82fa1782d9c6cb7b69696fd0d6638297f
dexter.jung <korcosin@gmail.com>
2019-03-17 14:52:21 +0900

    add chapter09 source code

--- CsharpBasic/RoadBook.CsharpBasic.Chapter09/Program.cs
+++ CsharpBasic/RoadBook.CsharpBasic.Chapter09/Program.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RoadBook.CsharpBasic.Chapter09
+﻿namespace RoadBook.CsharpBasic.Chapter09
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Examples.Ex001 ex = new Examples.Ex001();
+
+            ex.Run();
         }
     }
 }
